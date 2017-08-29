dfa367f793857b4b79145f5eebb4657c903cbfb6
Froi Nunez <froinunezm@gmail.com>
2017-08-29 08:47:11 -0400

    modular vue files added, jshint added to get rid of warnings, update on main.js file

--- src/main.js
+++ src/main.js
@@ -1,63 +1,43 @@
 import Vue from 'vue';
 import './style.scss';
 
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
+
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
 new Vue({
   el: '#app',
-  components: {
-    'movie-list': {
-      template:  `
-      <div id="movie-list">
-        <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
-      </div>`,
-      data() {
-        return {
-          movies: [
-            { title: 'Network' },
-            { title: 'Pulp Fiction' },
-            { title: 'Home Alone' }
-          ]
+  data: {
+    genre: [],
+    time: [],
+    movies: []
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      if (checked) {
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (index > -1) {
+          this[category].splice(index, 1);
         }
       }
-    },
-    'movie-filter': {
-      template:  `
-      <div id="movie-filter">
-        <h2>Filter Results</h2>
-        <check-filter></check-filter>
-      </div>`,
-      components: {
-        'check-filter': {
-          template:  `<div>Filter</div>`
-        }
-      },
     }
+  },
+  components: {
+    MovieList,
+    MovieFilter
+  },
+  created() {
+    this.$http.get('/api').then(response => {
+      this.movies = response.data;
+    });
   }
 });
 
-// Components help you extend basic HTMl elements to encapsulate reusable code.
-// Register a component:
-// Vue.component('my-component', {
-//  template: '<div>{{ msg }}</div>',
-//  data() {
-//    msg: 'A custom component!'
-//  }
+// var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+//   log: () => {},
+//   heartbeat: 2000
 // });
-//
-// A component can be thought if as a mini instance of Vue with it's own properties, methods etc. that are isolated only to that component
-
-
-
-// Now the component can be used by the root instance of Vue
-// <div id="example">
-//   <my-component></my-component>
-// </div>
-//
-// new Vue({ el: '#example' });
-//
-// Which renders as:
-// <div id="example">
-//   <div>A custom component!</div>
-// </div>
-
-
-//When creating a data property in vue components, the data must be a function that returns an object, otherwise, the console will throw an error. This is because components can be reused,
